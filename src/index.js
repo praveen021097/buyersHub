affac3f32995ce5a256f88f08680c5014286fafc
praveen021097 <96250697+praveen021097@users.noreply.github.com>
2024-01-12 20:22:17 +0530

    add url

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux';
 import { store } from './Redux/store';
 import { ToastContainer } from 'react-toastify';
 
-axios.defaults.baseURL="https://wowowo.cyclic.app"
+axios.defaults.baseURL="https://buyershubb.onrender.com"
 // axios.defaults.headers.post["Content-Type"]="application/json";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
